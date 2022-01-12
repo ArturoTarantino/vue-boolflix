969b08630725874996122af305269045b5625009
ArturoTarantino <arturotarantino92@gmail.com>
2022-01-12 17:00:03 +0100

    handling image path null and duplicate keys

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -20,27 +20,27 @@
             <li>Voto: {{ item.vote_average }}</li>
         </ul> -->
         <ul v-if="movie">
-            <li class="cover-box"><img class="cover" :src="imgPath + movie.backdrop_path" alt=""></li>
+            <li class="cover-box"><img v-if="movie.backdrop_path !== null" class="cover" :src="imgPath + movie.backdrop_path" alt=""></li>
             <li>Titolo: {{ movie.title }}</li>
             <li>Titolo Originale: {{ movie.original_title }}</li>
             <li>
                 Lingua: <img v-if="movie.original_language === 'en' || movie.original_language === 'it'" :src="require('../assets/' + movie.original_language + '.png')" alt="">
                         <span v-else>{{ movie.original_language }}</span>
             </li>
-            <li>Voto: <i v-for="n in getIntfrom1to5(movie.vote_average)" :key="n" class="fas fa-star yellow"></i> 
-                    <i v-for="n in (maxStars - getIntfrom1to5(movie.vote_average))" :key="n" class="far fa-star"></i>
+            <li>Voto: <span><i v-for="n in getIntfrom1to5(movie.vote_average)" :key="n" class="fas fa-star yellow"></i></span> 
+                    <span><i v-for="n in (maxStars - getIntfrom1to5(movie.vote_average))" :key="n" class="far fa-star"></i></span>
             </li>
         </ul>
         <ul v-else-if="series">
-            <li class="cover-box"><img class="cover" :src="imgPath + series.backdrop_path" alt=""></li>
+            <li class="cover-box"><img v-if="series.backdrop_path !== null" class="cover" :src="imgPath + series.backdrop_path" alt=""></li>
             <li>Titolo: {{ series.name }}</li>
             <li>Titolo Originale: {{ series.original_name }}</li>
             <li>
                 Lingua: <img v-if="series.original_language === 'en' || series.original_language === 'it'" :src="require('../assets/' + series.original_language + '.png')" alt="">
                         <span v-else>{{ series.original_language }}</span>
             </li>
-            <li>Voto: <i v-for="n in getIntfrom1to5(series.vote_average)" :key="n" class="fas fa-star yellow"></i>
-                    <i v-for="n in (maxStars - getIntfrom1to5(series.vote_average))" :key="n" class="far fa-star"></i>
+            <li>Voto: <span><i v-for="n in getIntfrom1to5(series.vote_average)" :key="n" class="fas fa-star yellow"></i></span>
+                    <span><i v-for="n in (maxStars - getIntfrom1to5(series.vote_average))" :key="n" class="far fa-star"></i></span>
             </li>
         </ul>
     </div>
@@ -93,6 +93,7 @@ export default {
         }
         .cover-box {
             text-align: center;
+            min-height: 183px;
             .cover {
             height: 170px;
         }
